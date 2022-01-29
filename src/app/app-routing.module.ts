ed10421d9eb09ebe936f7118dc940d673a98c424
silas-sousa <silassousadejesus@gmail.com>
2022-01-29 10:03:51 -0300

    pr do pedro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,8 @@ import { EmpreendViewComponent } from './cdd/classEmpreend/empreend-view/empreen
 import { GenPsiComponent } from './cdd/classPsi/gen-psi/gen-psi.component';
 import { TecViewComponent } from './cdd/classTec/tec.view/tec.view.component';
 import { FeedComponent } from './feed/feed.component';
-<<<<<<< HEAD
 import { ClassSugestaoComponent } from './cdd/class-sugestao/class-sugestao.component';
 
-=======
-import { BiblioteconomiaComponent } from './biblioteconomia/componente/biblioteconomia/biblioteconomia.component';
->>>>>>> 4e6174f3531b9dfd97166969cf797c66795b7639
 
 const enviarSemLogin = () => redirectUnauthorizedTo(['/app-app-cadastro']);
 
@@ -64,6 +60,11 @@ const routes: Routes = [
   },
   {
     path: 'sugestao', component: ClassSugestaoComponent
+  },
+  {
+    path: 'biblioteconomia',
+    loadChildren: () => import('./biblioteconomia/biblioteconomia.module').then(c => c.BiblioteconomiaModule),
+    ...canActivate(enviarSemLogin)
   }
 
 
